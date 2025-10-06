f02046254d003f0f2a21d6dc584cc35a20145860
VeronicaMartinezfnd <veronica.martinez7002@gmail.com>
2025-10-06 14:43:27 -0600

    Create Clase 03 - 03.py

---
+++ Semana 03/Clase 03 - 03.py
@@ -0,0 +1,27 @@
+"""year = 2025
+temperature = 28.5
+
+pi = 3.14159
+
+nota = 5.5
+aprobacion = 6
+
+validacion = nota != 5.5
+
+estado = nota > aprobacion
+
+grad = year + 5
+
+print(grad)
+print(temperature)
+print("pi")
+
+print(validacion)"""
+
+edad = 20
+temperatura = 27.5
+pi = 3.14159
+pi_entero = int(pi) # hago un cambio de tipo de dato a entero
+nombre = "Jose"
+
+print(pi_entero)
